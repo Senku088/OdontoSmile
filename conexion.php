cb3dea57d8e1a3fb65970b438e4991d46df9bf82
Francisco <frankdanielsanchez@gmail.com>
2023-10-06 13:56:32 -0600

    Todo el archivo PHP ya funciona, se cambio el nombre del HTML OdontoSmaile a registroOdonto y se modifico un poco los HTML para que funcionace mejor el PHP

--- conexion.php
+++ conexion.php
@@ -8,9 +8,17 @@
 
     }
 
-    
+    if(isset($_POST['botonRegistroO'])){
+        insertarOdonto($conexion);
+    }
+    if(isset($_POST['botonRegistroP'])){
+        insertarPaciente($conexion);
+    }
+    if(isset($_POST['botonInicioSesion'])){
+        iniciarSesion($conexion);
+    }
 
-    function insertar($conexion){
+    function insertarOdonto($conexion){
         $nombre = $_POST['nombreRegistroO'];
         $contrasena = $_POST['contrasenaRegistroO'];
         $especialidad = $_POST['especialidadRegistroO'];
@@ -29,7 +37,52 @@
         }
     }
 
-    insertar($conexion);
+    function insertarPaciente($conexion){
+        $nombre = $_POST['nombreRegistroP'];
+        $contrasena = $_POST['contrasenaRegistroP'];
+        $direccion = $_POST['direccionRegistroP'];
+        $correo = $_POST['correoRegistroP'];
+        $telefono = $_POST['telefonoRegistroP'];
+
+        $consulta = "USE agenda";
+        mysqli_query($conexion, $consulta);
+        $consulta = "INSERT INTO paciente (nombre, contrasena, direccion, correo, telefono) VALUES ('$nombre', '$contrasena', '$direccion', '$correo', '$telefono')";
+
+        if ($conexion->query($consulta) === TRUE){
+            echo "Exito";
+        } else {
+            echo "Error" . $conexion->Error;
+        }
+    }
+    function iniciarSesion($conexion){
+        $nombre = $_POST['userSesion'];
+        $contrasena = $_POST['passwordSesion'];
+
+        $consulta = "USE agenda";
+        mysqli_query($conexion, $consulta);
+        $consulta = "SELECT * FROM dentista WHERE nombre='$nombre'";
+
+        $resultado = mysqli_query($conexion, $consulta);
+
+        if(mysqli_num_rows($resultado) == 1){
+            $row = mysqli_fetch_assoc($resultado);
+            echo $row['nombre']." ".$row['contrasena']."\n".$nombre." ".$contrasena."\n";
+            if($contrasena == $row['contrasena']){
+                $_SESSION['nombre'] = $nombre;
+                header('location: AgendarCita.html');
+            } else {
+                echo "Contraseña incorrecta";
+            }
+        } else {
+            echo "Usuario no encontrado";
+        }
+
+        /*if ($conexion->query($consulta) === TRUE){
+            echo "Exito";
+        } else {
+            echo "Error" . $conexion->Error;
+        }*/
+    }
     
     mysqli_close($conexion);
 
