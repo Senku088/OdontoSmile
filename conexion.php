a599d1b850a7075e7a7bfddc853a237a21b196ec
Francisco <frankdanielsanchez@gmail.com>
2023-11-13 01:09:07 -0600

    Se hicieron los cambios con php y javascript con ajax

--- conexion.php
+++ conexion.php
@@ -1,5 +1,7 @@
 <?php
 
+    session_start();
+
     #Conexion a la base de datos aun en servidor local
     $conexion = mysqli_connect('127.0.0.1:3306', 'root', '', 'agenda');
     
@@ -23,6 +25,26 @@
     if(isset($_POST['botonAgregarTratamiento'])){
         insertarTratamiento($conexion);
     }
+    if(isset($_POST['funcion'])){
+        $funcion = $_POST['funcion'];
+        switch($funcion){
+            case 'mostrarNotificaciones':
+                mostrarNotificaciones($conexion);
+                break;
+            case 'mostrarPerfil':
+                mostrarPerfil($conexion);
+                break;
+            case 'mostrarCitas':
+                mostrarCitas($conexion);
+                break;
+            case 'mostrarSeguimiento':
+                mostrarSeguimiento($conexion);
+                break;
+            case 'mostrarTratamientos':
+                mostrarTratamientos($conexion);
+                break;
+        }
+    }
 
     #Declaracion de las funciones para insertar o consultar en la base de datos
     function insertarOdonto($conexion){
@@ -38,7 +60,7 @@
         #mysqli_query($conexion, $consulta);
 
         if ($conexion->query($consulta) === TRUE){
-            echo "Exito";
+            header('location: inicioSesion.html');
         } else {
             echo "Error" . $conexion->Error;
         }
@@ -55,7 +77,7 @@
         $consulta = "INSERT INTO paciente (nombre, contrasena, direccion, correo, telefono) VALUES ('$nombre', '$contrasena', '$direccion', '$correo', '$telefono')";
 
         if ($conexion->query($consulta) === TRUE){
-            echo "Exito";
+            header('location: inicioSesion.html');
         } else {
             echo "Error" . $conexion->Error;
         }
@@ -126,12 +148,276 @@
         $consulta = "INSERT INTO tratamientos (nombre, descripcion) VALUES ('$nombre', '$descripcion')";
         
         if ($conexion->query($consulta) === TRUE){
-            echo "Exito";
+            header('location: crudTratamientos.html');
         } else {
             echo "Error" . $conexion->Error;
         }
     }
+    function mostrarNotificaciones($conexion){
+        $nombreUsuario = $_SESSION['nombre'];
+
+        $consulta = "USE agenda";
+        mysqli_query($conexion, $consulta);
+        $consulta = "SELECT * FROM dentista WHERE nombre='$nombreUsuario'";
+        $resultado = mysqli_query($conexion, $consulta);
+
+        if(mysqli_num_rows($resultado) == 1){
+
+            $row = mysqli_fetch_assoc($resultado);
+            $idUsuario = $row['idDentista'];
+
+            $consulta = "USE agenda";
+            mysqli_query($conexion, $consulta);
+            $consulta = "SELECT * FROM notificaciones WHERE idDentista='$idUsuario'";
+            $resultado = mysqli_query($conexion, $consulta);
+
+            if(mysqli_num_rows($resultado) >= 1){
+
+                while( $row = mysqli_fetch_assoc($resultado)){
+                    $new_array[] = $row;
+                }
+                echo json_encode($new_array);
+
+            } else {
+
+                echo "No tienes notificaciones hasta ahora";
+
+            }
+
+        } else {
+            
+            $consulta = "USE agenda";
+            mysqli_query($conexion, $consulta);
+            $consulta = "SELECT * FROM paciente WHERE nombre='$nombreUsuario'";
+
+            $resultado = mysqli_query($conexion, $consulta);
+
+            if(mysqli_num_rows($resultado) == 1){
+
+                $row = mysqli_fetch_assoc($resultado);
+                $idUsuario = $row['idPaciente'];
+
+                $consulta = "USE agenda";
+                mysqli_query($conexion, $consulta);
+                $consulta = "SELECT * FROM notificaciones WHERE idPaciente='$idUsuario'";
+                $resultado = mysqli_query($conexion, $consulta);
+
+                if(mysqli_num_rows($resultado) >= 1){
+
+                    while( $row = mysqli_fetch_assoc($resultado)){
+                        $new_array[] = $row;
+                    }
+                    echo json_encode($new_array);
     
+                } else {
+    
+                    echo "No tienes notificaciones hasta ahora";
+    
+                }
+
+            }  else {
+
+                echo "Error al encontrar tu usuario";
+
+            }
+        }
+    }
+    function mostrarPerfil($conexion){
+        $nombreUsuario = $_SESSION['nombre'];
+        echo $nombreUsuario;
+
+        $consulta = "USE agenda";
+        mysqli_query($conexion, $consulta);
+        $consulta = "SELECT * FROM dentista WHERE nombre='$nombreUsuario'";
+        $resultado = mysqli_query($conexion, $consulta);
+
+        if(mysqli_num_rows($resultado) == 1){
+
+            while( $row = mysqli_fetch_assoc($resultado)){
+                $new_array[] = $row;
+            }
+            echo json_encode($new_array);
+
+        } else {
+            
+            $consulta = "USE agenda";
+            mysqli_query($conexion, $consulta);
+            $consulta = "SELECT * FROM paciente WHERE nombre='$nombreUsuario'";
+            $resultado = mysqli_query($conexion, $consulta);
+
+            if(mysqli_num_rows($resultado) == 1){
+
+                while( $row = mysqli_fetch_assoc($resultado)){
+                    $new_array[] = $row;
+                }
+                echo json_encode($new_array);
+
+            }  else {
+
+                echo "Que raro, no tienes informacion de tu perfil";
+
+            }
+        }
+    }
+    function mostrarCitas($conexion){
+        $nombreUsuario = $_SESSION['nombre'];
+
+        $consulta = "USE agenda";
+        mysqli_query($conexion, $consulta);
+        $consulta = "SELECT * FROM dentista WHERE nombre='$nombreUsuario'";
+        $resultado = mysqli_query($conexion, $consulta);
+
+        if(mysqli_num_rows($resultado) == 1){
+
+            $row = mysqli_fetch_assoc($resultado);
+            $idUsuario = $row['idDentista'];
+
+            $consulta = "USE agenda";
+            mysqli_query($conexion, $consulta);
+            $consulta = "SELECT * FROM citas WHERE idDentista='$idUsuario'";
+            $resultado = mysqli_query($conexion, $consulta);
+
+            if(mysqli_num_rows($resultado) >= 1){
+
+                while( $row = mysqli_fetch_assoc($resultado)){
+                    $new_array[] = $row;
+                }
+                echo json_encode($new_array);
+
+            } else {
+
+                echo "No tienes ninguna cita en este momento";
+
+            }
+
+        } else {
+            
+            $consulta = "USE agenda";
+            mysqli_query($conexion, $consulta);
+            $consulta = "SELECT * FROM paciente WHERE nombre='$nombreUsuario'";
+
+            $resultado = mysqli_query($conexion, $consulta);
+
+            if(mysqli_num_rows($resultado) == 1){
+
+                $row = mysqli_fetch_assoc($resultado);
+                $idUsuario = $row['idPaciente'];
+
+                $consulta = "USE agenda";
+                mysqli_query($conexion, $consulta);
+                $consulta = "SELECT * FROM citas WHERE idPaciente='$idUsuario'";
+                $resultado = mysqli_query($conexion, $consulta);
+
+                if(mysqli_num_rows($resultado) >= 1){
+
+                    while( $row = mysqli_fetch_assoc($resultado)){
+                        $new_array[] = $row;
+                    }
+                    echo json_encode($new_array);
+    
+                } else {
+    
+                    echo "No tienes ninguna cita en este momento";
+    
+                }
+
+            }  else {
+
+                echo "Error al encontrar tu usuario";
+
+            }
+        }
+    }
+    function mostrarSeguimiento($conexion){
+        $nombreUsuario = $_SESSION['nombre'];
+
+        $consulta = "USE agenda";
+        mysqli_query($conexion, $consulta);
+        $consulta = "SELECT * FROM dentista WHERE nombre='$nombreUsuario'";
+        $resultado = mysqli_query($conexion, $consulta);
+
+        if(mysqli_num_rows($resultado) == 1){
+
+            $row = mysqli_fetch_assoc($resultado);
+            $idUsuario = $row['idDentista'];
+
+            $consulta = "USE agenda";
+            mysqli_query($conexion, $consulta);
+            $consulta = "SELECT * FROM seguimientos WHERE idDentista='$idUsuario'";
+            $resultado = mysqli_query($conexion, $consulta);
+
+            if(mysqli_num_rows($resultado) >= 1){
+
+                while( $row = mysqli_fetch_assoc($resultado)){
+                    $new_array[] = $row;
+                }
+                echo json_encode($new_array);
+
+            } else {
+
+                echo "No tienes nada en tu seguimiento";
+
+            }
+
+        } else {
+            
+            $consulta = "USE agenda";
+            mysqli_query($conexion, $consulta);
+            $consulta = "SELECT * FROM paciente WHERE nombre='$nombreUsuario'";
+
+            $resultado = mysqli_query($conexion, $consulta);
+
+            if(mysqli_num_rows($resultado) == 1){
+
+                $row = mysqli_fetch_assoc($resultado);
+                $idUsuario = $row['idPaciente'];
+
+                $consulta = "USE agenda";
+                mysqli_query($conexion, $consulta);
+                $consulta = "SELECT * FROM seguimientos WHERE idPaciente='$idUsuario'";
+                $resultado = mysqli_query($conexion, $consulta);
+
+                if(mysqli_num_rows($resultado) >= 1){
+
+                    while( $row = mysqli_fetch_assoc($resultado)){
+                        $new_array[] = $row;
+                    }
+                    echo json_encode($new_array);
+    
+                } else {
+    
+                    echo "No tienes nada en tu seguimiento";
+    
+                }
+
+            }  else {
+
+                echo "Error al encontrar tu usuario";
+
+            }
+        }
+    }
+    function mostrarTratamientos($conexion){
+
+        $consulta = "USE agenda";
+        mysqli_query($conexion, $consulta);
+        $consulta = "SELECT * FROM tratamientos";
+        $resultado = mysqli_query($conexion, $consulta);
+
+        if(mysqli_num_rows($resultado) >= 1){
+
+            while( $row = mysqli_fetch_assoc($resultado)){
+                $new_array[] = $row;
+            }
+            echo json_encode($new_array);
+
+        } else {
+
+            echo "No hay tratamientos para mostrar";
+
+        }
+    }
+
     #Se cierra la conexion con la base de datos
     mysqli_close($conexion);
 
