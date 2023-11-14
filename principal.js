1766a7aaf87bf55e15e7ff338efac23cbcd95e1c
Sommer Sanchez <ssalmaisf@gmail.com>
2023-11-14 17:46:15 -0600

    accesibilidad

--- principal.js
+++ principal.js
@@ -10,3 +10,52 @@ contrasena.addEventListener('click', ()=>{
         divInputPass.appendChild(nuevoElemento);
     }
 });
+
+document.getElementById('buttonAccesibilidad').addEventListener('click', function() {
+    var accessibilityPanel = document.querySelector('.panelAccesibilidad');
+    accessibilityPanel.style.display = (accessibilityPanel.style.display === 'block') ? 'none' : 'block';
+    });
+
+    function invertColors() {
+    document.body.style.filter = "invert(100%)";
+    }
+
+    function applyFilter(filter) {
+    document.body.style.filter = filter;
+    }
+
+    function enableScreenReader() {
+    var screenReaderStatus = document.body.getAttribute('aria-live');
+
+        if(screenReaderStatus === 'assertive') {
+                // Si ya está activo, desactívalo
+                document.body.setAttribute('aria-live', 'off');
+                alert('Lector de pantalla desactivado.');
+        } else {
+            // Si está inactivo, actívalo
+            document.body.setAttribute('aria-live', 'assertive');
+            alert('Lector de pantalla activado. Puedes comenzar a utilizar tu lector de pantalla ahora.');
+        }
+    }
+
+    function toggleReadMode() {
+    var body = document.body;
+    body.classList.toggle('read-mode');
+
+    }
+
+    function changeFontSize(size) {
+        document.body.style.fontSize = size;
+    }
+
+    function changeLetterSpacing(spacing) {
+        document.body.style.letterSpacing = spacing;
+    }
+
+    function changeFontSize(value) {
+        document.body.style.fontSize = value + 'px';
+    }
+
+    function changeLetterSpacing(value) {
+        document.body.style.letterSpacing = value + 'px';
+    }
